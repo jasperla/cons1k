5ecb176868439158a0b0dd6e0c8305afd9f6c668
Jasper Lievisse Adriaanse <jasper@humppa.nl>
2013-02-13 13:33:08 +0100

    Rewrite to dynamically get the ldom information from the running host, instead of having it statically defined in a configuration file.

--- cons1k.sh
+++ cons1k.sh
@@ -3,7 +3,8 @@
 # Helper to make life easier dealing with multiple logical
 # domains.
 
-conf="cons1k.conf"
+# Use the dmesg on disk since the dmesg buffer may have overflowed since boot.
+dmesg="/var/run/dmesg.boot"
 
 if [ `arch -s` != "sparc64" ]; then
     echo "==> Error: host is not running OpenBSD/sparc64"
@@ -15,28 +16,22 @@ usage() {
     exit 1
 }
 
-if [ ! -r "${conf}" ]; then
-    echo "==> Error: cannor read config file: ${conf}."
-    exit 1
-fi
-
 if [ -z $1 ]; then
     usage
 else
-    grep -qw $1 ${conf} || usage
+    grep -qw ldom-$1 ${dmesg} || usage
     ldom=$1
 fi
 
-configuration=$(grep -w ${ldom} ${conf})
-tty=$(echo ${configuration} | awk '{print $2}')
-vnet=$(echo ${configuration} | awk '{print $3}')
+tty=$(grep domain\ \"${ldom}\" ${dmesg} | awk '{print $1}' | sed 's,vcctty,,g')
+tty="/dev/ttyV${tty}"
 
-if [ ! -r "/dev/${tty}" ]; then
-    echo "==> Error: /dev/${tty} does not exist."
+if [ ! -r "${tty}" ]; then
+    echo "==> Error: ${tty} does not exist."
     exit 1
 fi
 
 tmux new-session -s cons1k -d
-echo "==> Connecting to ${ldom} on /dev/${tty}"
-tmux neww -t cons1k -n ${ldom} "cu -l ${tty}"
+echo "==> Connecting to ${ldom} on ${tty}"
+tmux neww -t cons1k -n ${ldom} "sudo cu -l ${tty}"
 tmux attach-session -t cons1k
